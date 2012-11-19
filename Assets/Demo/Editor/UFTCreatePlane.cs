9628530daad8fe65ca19aee144e278b51cb9d23a
Nikolay <nicloay@gmail.com>
2012-11-19 23:15:37 +0800

    UFT-24 enhanced plane create wizard

--- Assets/Demo/Editor/UFTCreatePlane.cs
+++ Assets/Demo/Editor/UFTCreatePlane.cs
@@ -3,21 +3,26 @@ using System.Collections;
 using UnityEditor;
 
 public class UFTCreatePlane : ScriptableWizard {
+	public string meshName="plane";
 	public int width=256;
 	public int height=128;
+	public Material material;
+	public UFTAtlasMetadata atlasMetadata;
+	
 	
 	[MenuItem ("GameObject/Create Plane")]
     static void CreateWizard () {
-        ScriptableWizard.DisplayWizard<UFTCreatePlane>("Create Plane", "Create");
+        ScriptableWizard.DisplayWizard<UFTCreatePlane>("UFT Create Plane", "Create");
         //If you don't want to use the secondary button simply leave it out:
         //ScriptableWizard.DisplayWizard<WizardCreateLight>("Create Light", "Create");
     }
 	
 	 void OnWizardCreate () {
         GameObject go = MeshUtil.createPlane(width,height);
-		go.AddComponent<UFTSelectTextureFromAtlas>();
+		go.AddComponent<UFTSelectTextureFromAtlas>().atlasMetadata=atlasMetadata;
+		go.renderer.material=material;
 		
-		AssetDatabase.CreateAsset(go.GetComponent<MeshFilter>().sharedMesh, "Assets/plane.asset" );
+		AssetDatabase.CreateAsset(go.GetComponent<MeshFilter>().sharedMesh, AssetDatabase.GenerateUniqueAssetPath("Assets/"+meshName+".asset") );
         AssetDatabase.SaveAssets();
 		
     } 
