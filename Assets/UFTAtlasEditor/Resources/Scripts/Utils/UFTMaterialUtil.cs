a876b4f11ae62b500317fced4beb49f426f4c6f1
nicloay <nicloay@gmail.com>
2013-03-27 16:00:39 +0400

    list materials cleaning.

--- Assets/UFTAtlasEditor/Resources/Scripts/Utils/UFTMaterialUtil.cs
+++ Assets/UFTAtlasEditor/Resources/Scripts/Utils/UFTMaterialUtil.cs
@@ -14,19 +14,16 @@ internal enum ShaderPropertyType
 
 
 public class UFTMaterialUtil {
-	public static string SHADER_PROPERTY_TEXTURE_TYPE="TexEnv";
 	
 	public static List<Texture> getTextures(Material material){
-		Shader shader=material.shader;
+		Shader shader=material.shader;			
 		List<Texture> materials=new List<Texture>();
 		for (int i = 0; i < ShaderUtil.GetPropertyCount(shader) ; i++) {
 			Debug.Log("type=="+ShaderUtil.GetPropertyType(shader,i));
 			if (ShaderUtil.GetPropertyType(shader,i) == ShaderUtil.ShaderPropertyType.TexEnv){
-				Debug.Log("im here   =>"+ShaderUtil.GetPropertyName(shader,i));
 				materials.Add(material.GetTexture(ShaderUtil.GetPropertyName(shader,i)));
 			}			
 		}
 		return materials;
-	}
-	
+	}	
 }
